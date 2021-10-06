b9b84eb08da218403063ba9cc5512ffeb6372df8
Jakob Braun <jakob.braun@posteo.de>
2021-10-06 07:10:05 -0300

    improved tests

--- src/test/java/com/exasol/adapter/document/files/PerformanceIT.java
+++ src/test/java/com/exasol/adapter/document/files/PerformanceIT.java
@@ -1,16 +1,12 @@
 package com.exasol.adapter.document.files;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-import java.io.IOException;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import org.junit.jupiter.api.*;
 
 import com.exasol.adapter.document.files.s3testsetup.AwsS3TestSetup;
 import com.exasol.adapter.document.files.s3testsetup.S3TestSetup;
+import com.exasol.performancetestrecorder.PerformanceTestRecorder;
 
 class PerformanceIT {
     private static final S3TestSetup AWS_S3_TEST_SETUP = new AwsS3TestSetup();
@@ -26,15 +22,19 @@ class PerformanceIT {
         SETUP.close();
     }
 
-    @Test
-    void test() throws SQLException, IOException {
-        SETUP.getStatement().executeUpdate("ALTER SESSION SET SCRIPT_OUTPUT_ADDRESS = '127.0.0.1:3000';");
+    @RepeatedTest(5)
+    @Tag("regression")
+    void testLoadSalesParquetFiles(final TestInfo testInfo) throws Exception {
+        // SETUP.getStatement().executeUpdate("ALTER SESSION SET SCRIPT_OUTPUT_ADDRESS = '127.0.0.1:3000';");
         SETUP.createVirtualSchema("SALES_VS",
                 () -> getClass().getClassLoader().getResourceAsStream("performanceTestMapping.json"));
-        final ResultSet resultSet = SETUP.getStatement().executeQuery("SELECT COUNT(*) FROM SALES_VS.SALES_POSITION");
-        resultSet.next();
-        final long size = resultSet.getLong(1);
-        assertThat(size, equalTo(27363271L));
-        System.out.println("Size: " + resultSet.getLong(1));
+        PerformanceTestRecorder.getInstance().recordExecution(testInfo, () -> {
+            final ResultSet resultSet = SETUP.getStatement()
+                    .executeQuery("SELECT COUNT(*) FROM SALES_VS.SALES_POSITION");
+            resultSet.next();
+            final long size = resultSet.getLong(1);
+            // assertThat(size, equalTo(27363271L));
+            System.out.println("Size: " + size);
+        });
     }
 }

a5891a52db003934759096d572c78465a6c42ebc
Jakob Braun <jakob.braun@posteo.de>
2021-11-17 06:45:34 -0400

    changes from review

--- .ci/retry.sh
+++ .ci/retry.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-numRetries=30
-retryDelay=10
+numRetries=3
+retryDelay=1
 
 NUM=$numRetries
 until "$@"
 do
   1>&2 echo "failure ... retrying $NUM more times"
   sleep $retryDelay
-  ((NUM--))
-  if [ $NUM -eq 0 ]
+  NUM=$((NUM-1))
+  if [ "$NUM" -eq 0 ]
   then
     1>&2 echo "command was not successful after $numRetries tries"
     exit 1

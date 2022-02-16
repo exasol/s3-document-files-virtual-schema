81bcf7da732b6aff402f517462abca5faf7ac9f7
Jakob Braun <jakob.braun@posteo.de>
2022-02-16 04:31:54 -0400

    Updated dependencies

---
+++ doc/changes/changes_2.0.2.md
@@ -0,0 +1,13 @@
+# Virtual Schema for document data in files on AWS S3 2.0.2, released 2022-??-??
+
+Code name: Dependency Updates
+
+## Dependency Updates
+
+### Compile Dependency Updates
+
+* Updated `com.exasol:virtual-schema-common-document-files:5.0.0` to `6.0.1-SNAPSHOT`
+
+### Test Dependency Updates
+
+* Updated `com.exasol:virtual-schema-common-document-files:5.0.0` to `6.0.1-SNAPSHOT`
--- src/main/java/com/exasol/adapter/document/files/S3FileFinderFactory.java
+++ src/main/java/com/exasol/adapter/document/files/S3FileFinderFactory.java
@@ -1,20 +1,20 @@
 package com.exasol.adapter.document.files;
 
 import com.exasol.adapter.document.connection.ConnectionPropertiesReader;
-import com.exasol.adapter.document.documentfetcher.files.FileLoader;
-import com.exasol.adapter.document.documentfetcher.files.FileLoaderFactory;
+import com.exasol.adapter.document.documentfetcher.files.FileFinderFactory;
+import com.exasol.adapter.document.documentfetcher.files.RemoteFileFinder;
 import com.exasol.adapter.document.files.connection.S3ConnectionPropertiesReader;
 import com.exasol.adapter.document.files.stringfilter.StringFilter;
 
 /**
- * Factory for {@link S3FileLoader}s.
+ * Factory for {@link S3FileFinder}s.
  */
-public class S3FileLoaderFactory implements FileLoaderFactory {
+public class S3FileFinderFactory implements FileFinderFactory {
 
     @Override
-    public FileLoader getLoader(final StringFilter filePattern,
+    public RemoteFileFinder getFinder(final StringFilter filePattern,
             final ConnectionPropertiesReader connectionInformation) {
-        return new S3FileLoader(filePattern, new S3ConnectionPropertiesReader().read(connectionInformation));
+        return new S3FileFinder(filePattern, new S3ConnectionPropertiesReader().read(connectionInformation));
     }
 
     @Override

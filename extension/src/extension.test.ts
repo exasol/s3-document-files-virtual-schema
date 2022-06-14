9f8ebe00d6cef91b15dba1905c3e7980a03cdd31
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-06-14 15:09:30 +0200

    Fix running tests by upgrading the api

--- extension/src/extension.test.ts
+++ extension/src/extension.test.ts
@@ -2,13 +2,20 @@ import { describe, expect, it } from '@jest/globals';
 import { createExtension } from "./extension";
 
 function getInstalledExtension(): any {
-  return (global as any)["installedExtension"]
+  return (global as any).installedExtension
 }
 
 describe("Extension", () => {
+  
   it("creates an extension", () => {
     const ext = createExtension();
     expect(ext).not.toBeNull()
+  })
+
+  it("creates a new object for every call", () => {
+    const ext1 = createExtension();
+    const ext2 = createExtension();
+    expect(ext1).not.toBe(ext2)
   })
 
   it("registers when loaded", () => {

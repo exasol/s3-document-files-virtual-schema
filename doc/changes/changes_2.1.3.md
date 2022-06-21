811838393717184b7ba67e6e582c02866f7a25e5
ckunki <christoph.kuhnke@exasol.com>
2022-06-21 17:41:55 +0200

    feature/70: Added validation that awsEndpointOverride starts without protocol

---
+++ doc/changes/changes_2.1.3.md
@@ -0,0 +1,11 @@
+# Virtual Schema for document data in files on AWS S3 2.1.3, released 2022-??-??
+
+Code name:
+
+## Summary
+
+## Features
+
+* #70: Added validation that awsEndpointOverride starts without protocol
+
+## Dependency Updates
--- src/main/java/com/exasol/adapter/document/files/connection/S3ConnectionPropertiesReader.java
+++ src/main/java/com/exasol/adapter/document/files/connection/S3ConnectionPropertiesReader.java
@@ -1,8 +1,33 @@
 package com.exasol.adapter.document.files.connection;
 
 import com.exasol.adapter.document.connection.ConnectionPropertiesReader;
+import com.exasol.errorreporting.ExaError;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class S3ConnectionPropertiesReader {
+
+    private static final Pattern PROTOCOLS = Pattern.compile("^https?://");
+    public static final String AWS_ENDPOINT_OVERRIDE = "awsEndpointOverride";
+
+    private String readAwsEndpointOverride(final ConnectionPropertiesReader reader) {
+        final String raw = reader.readString(AWS_ENDPOINT_OVERRIDE).orElse(null);
+        if (raw == null) {
+            return null;
+        }
+        final Matcher matcher = PROTOCOLS.matcher(raw);
+        if (!matcher.find()) {
+            return raw;
+        }
+        throw new IllegalArgumentException(ExaError
+                .messageBuilder("E-S3VS-8")
+                .message("Property {{property}} has invalid value {{value}}."
+                                + " Value must not contain a protocol.",
+                        AWS_ENDPOINT_OVERRIDE, raw)
+                .mitigation("Please remove prefix {{prefix}}.", matcher.group(0))
+                .toString());
+    }
 
     public S3ConnectionProperties read(final ConnectionPropertiesReader reader) {
         return S3ConnectionProperties.builder()//
@@ -10,7 +35,7 @@ public class S3ConnectionPropertiesReader {
                 .awsAccessKeyId(reader.readRequiredString("awsAccessKeyId"))
                 .awsSecretAccessKey(reader.readRequiredString("awsSecretAccessKey"))
                 .awsRegion(reader.readRequiredString("awsRegion"))
-                .awsEndpointOverride(reader.readString("awsEndpointOverride").orElse(null))
+                .awsEndpointOverride(readAwsEndpointOverride(reader))
                 .awsSessionToken(reader.readString("awsSessionToken").orElse(null))
                 .s3PathStyleAccess(reader.readBooleanWithDefault("s3PathStyleAccess", false))
                 .s3Bucket(reader.readRequiredString("s3Bucket"))//

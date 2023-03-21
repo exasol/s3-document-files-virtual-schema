54779846d0afd9ea7598a5db4736a45c0fe64ee9
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-03-21 13:54:52 +0100

    Update references

--- src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
+++ src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
@@ -28,7 +28,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 public class IntegrationTestSetup implements AutoCloseable {
     public static final Path CLOUD_SETUP_CONFIG = Path.of("cloudSetup/generated/testConfig.json");
-    public static final String ADAPTER_JAR = "document-files-virtual-schema-dist-7.1.4-s3-2.4.2.jar";
+    public static final String ADAPTER_JAR = "document-files-virtual-schema-dist-7.2.0-s3-2.5.0.jar";
     public static final Path ADAPTER_JAR_LOCAL_PATH = Path.of("target", ADAPTER_JAR);
     public final String s3BucketName;
     private final ExasolTestSetup exasolTestSetup = new ExasolTestSetupFactory(CLOUD_SETUP_CONFIG).getTestSetup();

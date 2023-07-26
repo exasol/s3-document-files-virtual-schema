74f0a86f5f403bf21757cdfc1670777ea791bc4c
KK <christoph.kuhnke@exasol.com>
2023-07-26 10:40:20 +0200

    refactored S3RandomAccessInputStreamIT to enable execution with Minio, too

---
+++ src/test/java/com/exasol/adapter/document/files/itest/randomaccessinputstream/RandomAccessInputStreamLocalStackIT.java
@@ -0,0 +1,13 @@
+package com.exasol.adapter.document.files.itest.randomaccessinputstream;
+
+import com.exasol.adapter.document.files.s3testsetup.S3ContainerSetup;
+
+/**
+ * See parent class {@link AbstractRandomAccessInputStreamIT} for actual test cases which are generic for all test setup flavors
+ * implementing interface {@code S3ContainerSetup}.
+ */
+class RandomAccessInputStreamLocalStackIT extends AbstractRandomAccessInputStreamIT {
+    RandomAccessInputStreamLocalStackIT() {
+        super(S3ContainerSetup.localStack());
+    }
+}

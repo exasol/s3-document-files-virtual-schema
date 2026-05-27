ec6abc6041e54715a1c38b0d3c11b876b1f6ca75
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-05-27 10:23:50 +0200

    Use stricter linter rules

--- extension/eslint.config.mjs
+++ extension/eslint.config.mjs
@@ -6,5 +6,6 @@ import tseslint from 'typescript-eslint';
 
 export default defineConfig(
     js.configs.recommended,
-    tseslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic
 );
